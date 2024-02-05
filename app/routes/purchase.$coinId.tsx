88ee6f60c646744c096f2e544d9743dd7556d780
Vadim <vadim@koleoshkin.com>
2024-02-06 00:06:33 +0300

    Add purchase experiment

---
+++ app/routes/purchase.$coinId.tsx
@@ -0,0 +1,142 @@
+import { 
+    ActionFunctionArgs,
+    LoaderFunctionArgs,
+    MetaFunction,
+    json,
+    redirect
+} from "@remix-run/node"; // or cloudflare/deno
+
+import invariant from "tiny-invariant";
+
+import { getCoin, ICoin } from "../data";
+
+var buttonIndex: number = 0
+
+export const loader = async ({
+    params,
+  }: LoaderFunctionArgs) => {
+    invariant(params.coinId, "Missing contactId param");
+    const coin = getCoin(params.coinId)
+    if (coin == null) {
+        throw new Response("Oh no! Coin is not in the database", {
+            status: 404,
+        }); 
+    }
+    return json({ coin: coin, env: {
+        domain: process.env.DOMAIN
+    } });
+  };
+  
+
+export const meta: MetaFunction <typeof loader> = ({
+    data,
+  }) => {
+
+    if(data?.coin.id == null) {
+        throw Error('Coin not found')
+    }
+
+    let coin = getCoin(data.coin.id)
+
+    if (coin === null) {
+        throw Error('Coin not found')
+    }
+
+    return [
+        {
+          title: "Farcap" 
+        },
+        {
+          property: "og:title",
+          content: "Farcap Frame"
+        },
+        {
+          property: "og:image",
+          content: coin.preview
+        },
+        {
+          property: "fc:frame",
+          content: "vNext"
+        },
+        {
+          property: "fc:frame:image",
+          content : coin.preview
+        },
+        {
+          property: "fc:frame:button:1",
+          content: `Buy 0.01Ξ`
+        },
+        {
+          property: "fc:frame:button:1:action",
+          content: "post_redirect"
+        },
+        {
+            property: "fc:frame:button:2",
+            content: `Buy 0.1Ξ`
+        },
+        {
+            property: "fc:frame:button:2:action",
+            content: "post_redirect"
+        },
+        {
+            property: "fc:frame:button:3",
+            content: `Buy 1Ξ`
+        },
+        {
+            property: "fc:frame:button:3:action",
+            content: "post_redirect"
+        },
+        {
+          property: "fc:frame:button:4",
+          content: `🔄`
+        },
+        {
+          property: "fc:frame:button:4:action",
+          content: `post`
+        },
+        {
+          property: "fc:frame:post_url",
+          content: `${data.env.domain}/purchase/${coin.address}`
+        }
+      ];
+};
+
+export async function action({
+  request,
+  params
+}: ActionFunctionArgs) {
+    invariant(params.coinId, "Missing contactId param");
+    const body = await request.json();
+    buttonIndex = body.untrustedData.buttonIndex;
+  
+    const coin = getCoin(params.coinId)
+    if (coin == null) {
+        throw new Response("Oh no! Coin is not in the database", {
+            status: 404,
+        }); 
+    }
+    // if (buttonIndex == 1) {
+    //     return redirect(`${process.env.DOMAIN}/website/${coin.address}`, 302);
+    // } else 
+    if (buttonIndex == 1) {
+        return redirect(`${process.env.DOMAIN}/buy/${coin.address}?amount=0.01`, 302);
+    } else if(buttonIndex === 2) {
+        return redirect(`${process.env.DOMAIN}/buy/${coin.address}?amount=0.1`, 302);
+    } else if (buttonIndex == 3) {
+        return redirect(`${process.env.DOMAIN}/buy/${coin.address}?amount=1`, 302);
+    }
+
+  return (
+    <div>^_^</div>
+  ); 
+}
+
+export default function Purchase() {
+
+    return (
+        <div>
+            ^_^
+        </div>
+    );
+
+}
